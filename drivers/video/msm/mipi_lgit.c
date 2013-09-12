bca07c0a525bbee97d5f60e7d0c536b437d490d8
Naseer Ahmed <naseer@codeaurora.org>
2013-09-12 17:26:41 -0400

    mipi_lgit: Check for panel off at shutdown
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>
    
    Conflicts:
    	drivers/video/msm/mipi_lgit.c
    
    Change-Id: I7d5e2530ff8fd1ceaf45c4374b852109b9b9141f

--- drivers/video/msm/mipi_lgit.c
+++ drivers/video/msm/mipi_lgit.c
@@ -31,6 +31,7 @@ static struct msm_panel_common_pdata *mipi_lgit_pdata;
 static struct dsi_buf lgit_tx_buf;
 static struct dsi_buf lgit_rx_buf;
 static int __init mipi_lgit_lcd_init(void);
+static struct msm_fb_data_type *local_mfd;
 static int skip_init;
 
 #define DSV_ONBST 57
@@ -89,6 +90,7 @@ int mipi_lgit_lcd_ief_on(void)
 	int cnt = 0;
 	
 	mfd = platform_get_drvdata(pdev);
+	local_mfd = mfd;
 	if (!mfd)
 		return -ENODEV;
 	if (mfd->key != MFD_KEY)
@@ -224,6 +226,11 @@ int mipi_lgit_lcd_off_for_shutdown(void)
 static void mipi_lgit_lcd_shutdown(void)
 {
 	int ret = 0;
+
+	if(local_mfd && !local_mfd->panel_power_on) {
+		pr_info("%s:panel is already off\n", __func__);
+		return;
+	}
 
 	MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x10000000);
 	ret = mipi_dsi_cmds_tx(&lgit_tx_buf,
