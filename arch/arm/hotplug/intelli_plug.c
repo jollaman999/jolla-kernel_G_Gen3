f8222debb514094a466dbde155b68741a1ab0664
jollaman999 <admin@jollaman999.com>
2015-02-04 21:47:57 +0900

    intelli_plug: Set screen off max to 702MHz

--- arch/arm/hotplug/intelli_plug.c
+++ arch/arm/hotplug/intelli_plug.c
@@ -76,7 +76,7 @@ struct ip_cpu_info {
 
 static DEFINE_PER_CPU(struct ip_cpu_info, ip_info);
 
-static unsigned int screen_off_max = UINT_MAX;
+static unsigned int screen_off_max = 702000;
 module_param(screen_off_max, uint, 0644);
 
 #define CAPACITY_RESERVE	50
