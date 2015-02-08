a9f20403ada608ba9d2f4cc91ec30be57a9328a3
jollaman999 <admin@jollaman999.com>
2015-02-08 21:49:07 +0900

    Revert "mm: change invalidatepage prototype to accept length"
    This reverts commit 46045ec00647189a933f699dee1a3c4960cb4ca3.

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -64,8 +64,7 @@ static inline int ext4_begin_ordered_truncate(struct inode *inode,
 						   new_size);
 }
 
-static void ext4_invalidatepage(struct page *page, unsigned int offset,
-				unsigned int length);
+static void ext4_invalidatepage(struct page *page, unsigned long offset);
 static int noalloc_get_block_write(struct inode *inode, sector_t iblock,
 				   struct buffer_head *bh_result, int create);
 static int ext4_set_bh_endio(struct buffer_head *bh, struct inode *inode);
@@ -2542,8 +2541,7 @@ static int ext4_da_write_end(struct file *file,
 	return ret ? ret : copied;
 }
 
-static void ext4_da_invalidatepage(struct page *page, unsigned int offset,
-				   unsigned int length)
+static void ext4_da_invalidatepage(struct page *page, unsigned long offset)
 {
 	/*
 	 * Drop reserved blocks
@@ -2555,7 +2553,7 @@ static void ext4_da_invalidatepage(struct page *page, unsigned int offset,
 	ext4_da_page_release_reservation(page, offset);
 
 out:
-	ext4_invalidatepage(page, offset, length);
+	ext4_invalidatepage(page, offset);
 
 	return;
 }
@@ -2701,8 +2699,7 @@ static void ext4_invalidatepage_free_endio(struct page *page, unsigned long offs
 	} while (bh != head);
 }
 
-static void ext4_invalidatepage(struct page *page, unsigned int offset,
-				unsigned int length)
+static void ext4_invalidatepage(struct page *page, unsigned long offset)
 {
 	journal_t *journal = EXT4_JOURNAL(page->mapping->host);
 
@@ -2722,7 +2719,7 @@ static void ext4_invalidatepage(struct page *page, unsigned int offset,
 	if (journal)
 		jbd2_journal_invalidatepage(journal, page, offset);
 	else
-		block_invalidatepage(page, offset, PAGE_CACHE_SIZE - offset);
+		block_invalidatepage(page, offset);
 }
 
 static int ext4_releasepage(struct page *page, gfp_t wait)
