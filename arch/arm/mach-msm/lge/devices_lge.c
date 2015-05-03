e53c4139888c0a0536cc4740f19cf40c591b7806
jollaman999 <admin@jollaman999.com>
2015-05-03 13:44:07 +0900

    kexec: Hardboot support for j1 boards

--- arch/arm/mach-msm/lge/devices_lge.c
+++ arch/arm/mach-msm/lge/devices_lge.c
@@ -15,6 +15,7 @@
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/memory.h>
+#include <linux/persistent_ram.h>
 
 #include <asm/setup.h>
 #include <asm/sizes.h>
@@ -27,12 +28,18 @@
 
 #include <mach/board_lge.h>
 
+#include <linux/platform_data/ram_console.h>
+
 #ifdef CONFIG_LGE_PM
 #include CONFIG_BOARD_HEADER_FILE
 #endif
 
 #ifdef CONFIG_LGE_BOOT_TIME_CHECK
 #include "lge_boot_time_checker.h"
+#endif
+
+#ifdef CONFIG_KEXEC_HARDBOOT
+#include <linux/memblock.h>
 #endif
 
 /* setting whether uart console is enalbed or disabled */
@@ -414,69 +421,97 @@ int lge_get_factory_boot(void)
 	return res;
 }
 
+#ifdef CONFIG_ANDROID_PERSISTENT_RAM
+static struct persistent_ram_descriptor pram_descs[] = {
 #ifdef CONFIG_ANDROID_RAM_CONSOLE
-static struct resource ram_console_resource[] = {
 	{
 		.name = "ram_console",
-		.flags = IORESOURCE_MEM,
-	}
+		.size = LGE_RAM_CONSOLE_SIZE,
+	},
+#endif
+#ifdef CONFIG_LGE_HANDLE_PANIC
+	{
+		.name = "panic-handler",
+		.size = LGE_CRASH_LOG_SIZE,
+	},
+#endif
+};
+
+static struct persistent_ram lge_persistent_ram = {
+	.size = LGE_PERSISTENT_RAM_SIZE,
+	.num_descs = ARRAY_SIZE(pram_descs),
+	.descs = pram_descs,
+};
+
+void __init lge_add_persistent_ram(void)
+{
+	struct persistent_ram *pram = &lge_persistent_ram;
+	struct membank* bank = &meminfo.bank[0];
+
+	pram->start = bank->start + bank->size - LGE_PERSISTENT_RAM_SIZE;
+
+	persistent_ram_early_init(pram);
+}
+#endif
+
+void __init lge_reserve(void)
+{
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page, just before the ram_console
+	struct membank* bank = &meminfo.bank[0];
+	phys_addr_t start = bank->start + bank->size - SZ_1M - LGE_PERSISTENT_RAM_SIZE;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
+
+	lge_add_persistent_ram();
+}
+
+#ifdef CONFIG_ANDROID_RAM_CONSOLE
+static char bootreason[128] = {0,};
+int __init lge_boot_reason(char *s)
+{
+	int n;
+
+	if (*s == '=')
+		s++;
+	n = snprintf(bootreason, sizeof(bootreason),
+		 "Boot info:\n"
+		 "Last boot reason: %s\n", s);
+	bootreason[n] = '\0';
+	return 1;
+}
+__setup("bootreason", lge_boot_reason);
+
+struct ram_console_platform_data ram_console_pdata = {
+	.bootinfo = bootreason,
 };
 
 static struct platform_device ram_console_device = {
 	.name = "ram_console",
 	.id = -1,
-	.num_resources = ARRAY_SIZE(ram_console_resource),
-	.resource = ram_console_resource,
+	.dev = {
+		.platform_data = &ram_console_pdata,
+	}
 };
 
 void __init lge_add_ramconsole_devices(void)
 {
-	struct resource* res = ram_console_resource;
-	struct membank* bank = &meminfo.bank[0];
-
-	res->start = PHYS_OFFSET + bank->size;
-	res->end = res->start + LGE_RAM_CONSOLE_SIZE - 1;
-
-	printk(KERN_INFO "RAM CONSOLE START ADDR : %X\n", res->start);
-	printk(KERN_INFO "RAM CONSOLE END ADDR   : %X\n", res->end);
-
 	platform_device_register(&ram_console_device);
 }
 #endif // CONFIG_ANDROID_RAM_CONSOLE
 
 #ifdef CONFIG_LGE_HANDLE_PANIC
-static struct resource crash_log_resource[] = {
-	{
-		.name = "crash_log",
-		.flags = IORESOURCE_MEM,
-	}
-};
-
 static struct platform_device panic_handler_device = {
 	.name = "panic-handler",
-	.num_resources = ARRAY_SIZE(crash_log_resource),
-	.resource = crash_log_resource,
-	.dev = {
-		.platform_data = NULL,
-	}
+	.id = -1,
 };
-
-volatile resource_size_t lge_add_info;
 
 void __init lge_add_panic_handler_devices(void)
 {
-	struct resource* res = crash_log_resource;
-	struct membank* bank = &meminfo.bank[0];
-
-	res->start = bank->start + bank->size + LGE_RAM_CONSOLE_SIZE;
-	res->end = res->start + LGE_CRASH_LOG_SIZE - 1;
-
-	lge_add_info = res->start + LGE_CRASH_LOG_SIZE + (1024 * 2);
-
-	printk(KERN_INFO "CRASH LOG START ADDR : %X\n", res->start);
-	printk(KERN_INFO "CRASH LOG END ADDR   : %X\n", res->end);
-	printk(KERN_INFO "ADDITIONAL INFO ADDR : %X\n", lge_add_info);
-
 	platform_device_register(&panic_handler_device);
 }
 #endif // CONFIG_LGE_HANDLE_PANIC
