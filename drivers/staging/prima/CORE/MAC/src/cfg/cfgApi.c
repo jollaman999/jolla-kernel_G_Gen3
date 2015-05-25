69b2a90f23695d09222b4d573915e16a2e1453fe
jollaman999 <admin@jollaman999.com>
2015-05-25 15:03:46 +0900

    Revert "prima: release v3.2.3.7"
    This reverts commit 57b1c29a0a8c61762b224801ea4eed6ca4f165dc.

--- drivers/staging/prima/CORE/MAC/src/cfg/cfgApi.c
+++ drivers/staging/prima/CORE/MAC/src/cfg/cfgApi.c
@@ -74,22 +74,12 @@ static void Notify(tpAniSirGlobal, tANI_U16, tANI_U32);
 // ---------------------------------------------------------------------
 tANI_U32 cfgNeedRestart(tpAniSirGlobal pMac, tANI_U16 cfgId)
 {
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
-        return 0;
-    }
     return !!(pMac->cfg.gCfgEntry[cfgId].control & CFG_CTL_RESTART) ;
 }
 
 // ---------------------------------------------------------------------
 tANI_U32 cfgNeedReload(tpAniSirGlobal pMac, tANI_U16 cfgId)
 {
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
-        return 0;
-    }
     return !!(pMac->cfg.gCfgEntry[cfgId].control & CFG_CTL_RELOAD) ;
 }
 
@@ -194,12 +184,6 @@ cfgSetInt(tpAniSirGlobal pMac, tANI_U16 cfgId, tANI_U32 value)
         return eSIR_CFG_INVALID_ID;
     }
 
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
-        return eSIR_CFG_INVALID_ID;
-    }
-
     control  = pMac->cfg.gCfgEntry[cfgId].control;
     index    = control & CFG_BUF_INDX_MASK;
     retVal   = eSIR_SUCCESS;
@@ -275,12 +259,6 @@ cfgCheckValid(tpAniSirGlobal pMac, tANI_U16 cfgId)
         return(eSIR_CFG_INVALID_ID);
     }
 
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
-        return eSIR_CFG_INVALID_ID;
-    }
-
     control = pMac->cfg.gCfgEntry[cfgId].control;
 
     // Check if parameter is valid
@@ -326,12 +304,6 @@ wlan_cfgGetInt(tpAniSirGlobal pMac, tANI_U16 cfgId, tANI_U32 *pValue)
         PELOGE(cfgLog(pMac, LOGE, FL("Invalid cfg id %d"), cfgId);)
         retVal = eSIR_CFG_INVALID_ID;
         return retVal;
-    }
-
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
-        return eSIR_CFG_INVALID_ID;
     }
 
     control = pMac->cfg.gCfgEntry[cfgId].control;
@@ -396,12 +368,6 @@ cfgIncrementInt(tpAniSirGlobal pMac, tANI_U16 cfgId, tANI_U32 value)
     {
         PELOGE(cfgLog(pMac, LOGE, FL("Invalid cfg id %d"), cfgId);)
         retVal = eSIR_CFG_INVALID_ID;
-    }
-
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
-        return eSIR_CFG_INVALID_ID;
     }
 
     control  = pMac->cfg.gCfgEntry[cfgId].control;
@@ -499,12 +465,6 @@ cfgSetStrNotify(tpAniSirGlobal pMac, tANI_U16 cfgId, tANI_U8 *pStr,
     if (cfgId >= CFG_PARAM_MAX_NUM)
     {
         PELOGE(cfgLog(pMac, LOGE, FL("Invalid cfg id %d"), cfgId);)
-        return eSIR_CFG_INVALID_ID;
-    }
-
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
         return eSIR_CFG_INVALID_ID;
     }
 
@@ -688,12 +648,6 @@ wlan_cfgGetStrMaxLen(tpAniSirGlobal pMac, tANI_U16 cfgId, tANI_U32 *pLength)
         retVal = eSIR_CFG_INVALID_ID;
     }
 
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
-        return eSIR_CFG_INVALID_ID;
-    }
-
     control  = pMac->cfg.gCfgEntry[cfgId].control;
     index    = control & CFG_BUF_INDX_MASK;
     retVal   = eSIR_SUCCESS;
@@ -753,12 +707,6 @@ wlan_cfgGetStrLen(tpAniSirGlobal pMac, tANI_U16 cfgId, tANI_U32 *pLength)
     {
         PELOGE(cfgLog(pMac, LOGE, FL("Invalid cfg id %d"), cfgId);)
         retVal = eSIR_CFG_INVALID_ID;
-    }
-
-    if (!pMac->cfg.gCfgEntry)
-    {
-        PELOGE(cfgLog(pMac, LOGE, FL("gCfgEntry is NULL"));)
-        return eSIR_CFG_INVALID_ID;
     }
 
     control  = pMac->cfg.gCfgEntry[cfgId].control;
