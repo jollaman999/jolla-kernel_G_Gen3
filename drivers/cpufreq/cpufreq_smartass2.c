0b735923ee26428a698b4b7161563e1fb924a0e5
jollaman999 <admin@jollaman999.com>
2015-07-13 00:38:15 +0900

    cpufreq: Adjust default values of smartmax & smartass2

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -45,7 +45,7 @@
  * towards the ideal frequency and slower after it has passed it. Similarly,
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
-#define DEFAULT_AWAKE_IDEAL_FREQ 594000
+#define DEFAULT_AWAKE_IDEAL_FREQ 702000
 static unsigned int awake_ideal_freq;
 
 /*
@@ -62,7 +62,7 @@ static unsigned int sleep_ideal_freq;
  * Zero disables and causes to always jump straight to max frequency.
  * When below the ideal freqeuncy we always ramp up to the ideal freq.
  */
-#define DEFAULT_RAMP_UP_STEP 108000
+#define DEFAULT_RAMP_UP_STEP 216000
 static unsigned int ramp_up_step;
 
 /*
@@ -70,26 +70,26 @@ static unsigned int ramp_up_step;
  * Zero disables and will calculate ramp down according to load heuristic.
  * When above the ideal freqeuncy we always ramp down to the ideal freq.
  */
-#define DEFAULT_RAMP_DOWN_STEP 216000
+#define DEFAULT_RAMP_DOWN_STEP 108000
 static unsigned int ramp_down_step;
 
 /*
  * CPU freq will be increased if measured load > max_cpu_load;
  */
-#define DEFAULT_MAX_CPU_LOAD 90
+#define DEFAULT_MAX_CPU_LOAD 80
 static unsigned long max_cpu_load;
 
 /*
  * CPU freq will be decreased if measured load < min_cpu_load;
  */
-#define DEFAULT_MIN_CPU_LOAD 35
+#define DEFAULT_MIN_CPU_LOAD 30
 static unsigned long min_cpu_load;
 
 /*
  * The minimum amount of time to spend at a frequency before we can ramp up.
  * Notice we ignore this when we are below the ideal frequency.
  */
-#define DEFAULT_UP_RATE_US 30000;
+#define DEFAULT_UP_RATE_US 25000;
 static unsigned long up_rate_us;
 
 /*
@@ -103,7 +103,7 @@ static unsigned long down_rate_us;
  * The frequency to set when waking up from sleep.
  * When sleep_ideal_freq=0 this will have no effect.
  */
-#define DEFAULT_SLEEP_WAKEUP_FREQ 1026000
+#define DEFAULT_SLEEP_WAKEUP_FREQ 1134000
 static unsigned int sleep_wakeup_freq;
 
 /*
