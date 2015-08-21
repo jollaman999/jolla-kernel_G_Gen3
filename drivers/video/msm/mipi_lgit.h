613742492fcd9145fa2df0c5de5da7718a0ab8c7
jollaman999 <admin@jollaman999.com>
2015-08-21 23:44:45 +0900

    mipi_lgit: Fix sources
    Change-Id: I5365cebb86eafc609afc11441497e31b7757b803

--- drivers/video/msm/mipi_lgit.h
+++ drivers/video/msm/mipi_lgit.h
@@ -34,7 +34,6 @@
 
 int mipi_lgit_device_register(struct msm_panel_info *pinfo,
 					u32 channel, u32 panel);
-int mipi_lgit_lcd_on(struct platform_device *pdev);
 int mipi_lgit_lcd_off(struct platform_device *pdev);
 
 int mipi_lgit_lcd_off_for_shutdown(void);
