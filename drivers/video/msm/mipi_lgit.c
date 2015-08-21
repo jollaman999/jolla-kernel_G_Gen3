526ddcd85aeb79b19475de0aeaab2f1662510e1b
jollaman999 <admin@jollaman999.com>
2015-08-22 01:18:57 +0900

    mipi_lgit: Fix build error
    Change-Id: If7e089f06c3aeaf28a1379bcb975d47c26dfa9c1

--- drivers/video/msm/mipi_lgit.c
+++ drivers/video/msm/mipi_lgit.c
@@ -125,13 +125,13 @@ int mipi_lgit_lcd_ief_on(void)
 
 	if(!skip_init){
 		MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x10000000);
-		ret = mipi_dsi_cmds_tx(&lgit_tx_buf,
+		cnt = mipi_dsi_cmds_tx(&lgit_tx_buf,
 				mipi_lgit_pdata->power_on_set_2,
 				mipi_lgit_pdata->power_on_set_size_2);
 		MIPI_OUTP(MIPI_DSI_BASE + 0x38, 0x14000000);
-		if (ret < 0) {
+		if (cnt < 0) {
 			pr_err("%s: failed to transmit power_on_set_2 cmds\n", __func__);
-			return ret;
+			return cnt;
 		}
 	}
 	skip_init = false;
