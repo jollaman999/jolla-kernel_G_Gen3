af029ba1aaeb77f1dadc4768d2078b3509825b1a
jollaman999 <admin@jollaman999.com>
2015-11-11 02:52:03 +0900

    Revert "scroff_volctr: Temporary disable when music stopped with delay"
    This reverts commit 66eb8bb72ced128f3a3e67bd39352b08e4a4c199.

--- sound/soc/codecs/wcd9310.c
+++ sound/soc/codecs/wcd9310.c
@@ -51,23 +51,11 @@ bool wcd9310_is_playing;
 EXPORT_SYMBOL(wcd9310_is_playing);
 #endif
 
-// scroff_volctr: Temporary disable when music stopped with delay
+// scroff_volctr: Temporary disable when music stopped
 // - jollaman999 -
 #ifdef CONFIG_TOUCHSCREEN_SCROFF_VOLCTR
-#include <linux/workqueue.h>
-#define SOVC_DISABLE_DELAY	5000	// Temporary disable sovc after this time
 extern int sovc_tmp_onoff;
-static bool sovc_play_again = false;
-
-static void scroff_volctr_disable(struct work_struct *scroff_volctr_disable_work)
-{
-	if (sovc_play_again)
-		return;
-
-	sovc_tmp_onoff = 0;
-}
-static DECLARE_DELAYED_WORK(scroff_volctr_disable_work, scroff_volctr_disable);
-#endif /* CONFIG_TOUCHSCREEN_SCROFF_VOLCTR */
+#endif
 
 #define WCD9310_RATES (SNDRV_PCM_RATE_8000 | SNDRV_PCM_RATE_16000 |\
 			SNDRV_PCM_RATE_32000 | SNDRV_PCM_RATE_48000 |\
@@ -4236,10 +4224,9 @@ static int tabla_startup(struct snd_pcm_substream *substream,
 #ifdef CONFIG_INTELLI_PLUG
 	wcd9310_is_playing = true;
 #endif
-	// scroff_volctr: Temporary disable when music stopped with delay
+	// scroff_volctr: Temporary disable when music stopped
 	// - jollaman999 -
 #ifdef CONFIG_TOUCHSCREEN_SCROFF_VOLCTR
-	sovc_play_again = true;
 	sovc_tmp_onoff = 1;
 #endif
 
@@ -4300,12 +4287,10 @@ static void tabla_shutdown(struct snd_pcm_substream *substream,
 #ifdef CONFIG_INTELLI_PLUG
 	wcd9310_is_playing = false;
 #endif
-	// scroff_volctr: Temporary disable when music stopped with delay
+	// scroff_volctr: Temporary disable when music stopped
 	// - jollaman999 -
 #ifdef CONFIG_TOUCHSCREEN_SCROFF_VOLCTR
-	sovc_play_again = false;
-	schedule_delayed_work(&scroff_volctr_disable_work, SOVC_DISABLE_DELAY);
-	pr_info("[scroff_volctr]: SOVC Temporary Disabled.\n");
+	sovc_tmp_onoff = 0;
 #endif
 }
 
